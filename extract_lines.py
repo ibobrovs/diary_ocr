a0c0dbb32ae0257e6e6d17d31ac36efba1ce937e
Ivan Bobrov <ibobrovs9@gmail.com>
2025-07-05 18:50:10 +0300

    clustering by y

--- extract_lines.py
+++ extract_lines.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 
-# Папки
 INPUT_DIR = "pages"
 OUTPUT_DIR = "dataset/images"
 os.makedirs(OUTPUT_DIR, exist_ok=True)
@@ -19,13 +18,17 @@ for filename in sorted(os.listdir(INPUT_DIR)):
     # Бинаризация
     _, binary = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
+    # Морфология (шире по X, как ты хотел)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (60, 5))  # было 40x5
+    dilated = cv2.dilate(binary, kernel, iterations=1)
+
     # Контуры
-    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=lambda c: cv2.boundingRect(c)[1])
+    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=lambda c: cv2.boundingRect(c)[1])  # сортировка по Y
 
     for c in contours:
         x, y, w, h = cv2.boundingRect(c)
-        if h > 20 and w > 100:
+        if h > 20 and w > 150:
             line_img = image[y:y+h, x:x+w]
             output_path = os.path.join(OUTPUT_DIR, f"line_{line_counter:04d}.png")
             cv2.imwrite(output_path, line_img)
